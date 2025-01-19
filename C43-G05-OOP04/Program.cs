f7c1be1a2280e474a4841fa69feae45124aca8a8
sayed123 <sayedmohameddev@gmai.com>
2025-01-19 20:51:40 +0200

    Part02-04

--- C43-G05-OOP04/Program.cs
+++ C43-G05-OOP04/Program.cs
@@ -20,19 +20,19 @@ namespace C43_G05_OOP04
 
             public Duration(int totalSeconds)
             {
-                Hours = totalSeconds / 3600; 
-                totalSeconds %= 3600; 
-                Minutes = totalSeconds / 60; 
-                Seconds = totalSeconds % 60; 
+                Hours = totalSeconds / 3600;
+                totalSeconds %= 3600;
+                Minutes = totalSeconds / 60;
+                Seconds = totalSeconds % 60;
             }
 
             public Duration(double minutes)
             {
-                int totalSeconds = (int)(minutes * 60); 
-                Hours = totalSeconds / 3600; 
-                totalSeconds %= 3600; 
-                Minutes = totalSeconds / 60; 
-                Seconds = totalSeconds % 60; 
+                int totalSeconds = (int)(minutes * 60);
+                Hours = totalSeconds / 3600;
+                totalSeconds %= 3600;
+                Minutes = totalSeconds / 60;
+                Seconds = totalSeconds % 60;
             }
 
             public override string ToString()
@@ -46,19 +46,99 @@ namespace C43_G05_OOP04
                     return $"Minutes :{Minutes}, Seconds :{Seconds}";
                 }
             }
+
+            public static Duration operator +(Duration d1, Duration d2)
+            {
+                int totalSeconds = d1.ToSeconds() + d2.ToSeconds();
+                return new Duration(totalSeconds);
+            }
+
+            public static Duration operator +(int seconds, Duration d1)
+            {
+                int totalSeconds = d1.ToSeconds() + seconds;
+                return new Duration(totalSeconds);
+            }
+
+            public static Duration operator -(Duration d1, Duration d2)
+            {
+                int totalSeconds = d1.ToSeconds() - d2.ToSeconds();
+                return new Duration(totalSeconds);
+            }
+
+            public static Duration operator ++(Duration d)
+            {
+                int totalSeconds = d.ToSeconds() + 60;
+                return new Duration(totalSeconds);
+            }
+
+            public static Duration operator --(Duration d)
+            {
+                int totalSeconds = d.ToSeconds() - 60;
+                return new Duration(totalSeconds);
+            }
+
+            public static bool operator >(Duration d1, Duration d2)
+            {
+                return d1.ToSeconds() > d2.ToSeconds();
+            }
+
+            public static bool operator <(Duration d1, Duration d2)
+            {
+                return d1.ToSeconds() < d2.ToSeconds();
+            }
+
+            public static bool operator >=(Duration d1, Duration d2)
+            {
+                return d1.ToSeconds() >= d2.ToSeconds();
+            }
+
+            public static bool operator <=(Duration d1, Duration d2)
+            {
+                return d1.ToSeconds() <= d2.ToSeconds();
+            }
+
+            public static explicit operator DateTime(Duration d)
+            {
+                return new DateTime(1, 1, 1, d.Hours, d.Minutes, d.Seconds);
+            }
+
+            private int ToSeconds()
+            {
+                return (Hours * 3600) + (Minutes * 60) + Seconds;
+            }
         }
 
         static void Main(string[] args)
         {
             Duration D1 = new Duration(1, 10, 15);
-            Console.WriteLine(D1.ToString()); 
             Duration D2 = new Duration(3600);
-            Console.WriteLine(D2.ToString());  
+            Duration D3 = D1 + D2;
+            Console.WriteLine(D3);
 
-            Duration D3 = new Duration(7800);
-            Console.WriteLine(D3.ToString());  
-            Duration D4 = new Duration(666);
-            Console.WriteLine(D4.ToString());
-        }
+            D3 = D1 + 7800;
+            Console.WriteLine(D3);
+
+            D3 = 666 + D1;
+            Console.WriteLine(D3);
+
+            D3 = ++D1;
+            Console.WriteLine(D3);
+
+            D3 = --D2;
+            Console.WriteLine(D3);
+
+            D1 = D1 - D2;
+            Console.WriteLine(D1);
+
+            if (D1 > D2)
+                Console.WriteLine("D1 is greater than D2");
+
+            if (D1 <= D2)
+                Console.WriteLine("D1 is less than or equal to D2");
+
+            DateTime dateTime = (DateTime)D1;
+            Console.WriteLine(dateTime);
+        
+    }
     }
 }
