05c68fdff6fab71cf744e1e56dab222cb255058e
sayed123 <sayedmohameddev@gmai.com>
2025-01-19 20:27:16 +0200

    Q-04

--- C43-G05-OOP04/Program.cs
+++ C43-G05-OOP04/Program.cs
@@ -32,33 +32,53 @@ namespace C43_G05_OOP04
     //}
 
 
-    class ComplexNumber
+    //class ComplexNumber
+    //{
+    //    public double Real { get; set; }
+    //    public double Imaginary { get; set; }
+
+    //    public ComplexNumber(double real, double imaginary)
+    //    {
+    //        Real = real;
+    //        Imaginary = imaginary;
+    //    }
+
+    //    public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+    //    {
+    //        return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+    //    }
+
+    //    public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+    //    {
+    //        return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        string sign = Imaginary >= 0 ? "+" : "-";
+    //        return $"{Real} {sign} {Math.Abs(Imaginary)}i";
+    //    }
+    //}
+
+    class Employee
     {
-        public double Real { get; set; }
-        public double Imaginary { get; set; }
-
-        public ComplexNumber(double real, double imaginary)
+        public virtual void Work()
         {
-            Real = real;
-            Imaginary = imaginary;
-        }
-
-        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
-        {
-            return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
-        }
-
-        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
-        {
-            return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
-        }
-
-        public override string ToString()
-        {
-            string sign = Imaginary >= 0 ? "+" : "-";
-            return $"{Real} {sign} {Math.Abs(Imaginary)}i";
+            Console.WriteLine("Employee is working");
         }
     }
+
+    class Manager : Employee
+    {
+        public override void Work()
+        {
+            base.Work();
+
+            Console.WriteLine("Manager is managing");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -73,14 +93,18 @@ namespace C43_G05_OOP04
             //    Rectangle rect3 = new Rectangle(15);
             //    rect3.DisplayDimensions(); 
 
-            ComplexNumber c1 = new ComplexNumber(3, 4);
-            ComplexNumber c2 = new ComplexNumber(1, 2);
+            //ComplexNumber c1 = new ComplexNumber(3, 4);
+            //ComplexNumber c2 = new ComplexNumber(1, 2);
 
-            ComplexNumber sum = c1 + c2;
-            Console.WriteLine($"Sum: {sum}"); 
+            //ComplexNumber sum = c1 + c2;
+            //Console.WriteLine($"Sum: {sum}"); 
 
-            ComplexNumber difference = c1 - c2;
-            Console.WriteLine($"Difference: {difference}");
+            //ComplexNumber difference = c1 - c2;
+            //Console.WriteLine($"Difference: {difference}");
+
+            Manager manager = new Manager();
+
+            manager.Work();
         }
     }
 }
