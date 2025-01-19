5fbaacf521cda7bb82f0f0bec3a251e81f761343
sayed123 <sayedmohameddev@gmai.com>
2025-01-19 20:23:23 +0200

    Q-03

--- C43-G05-OOP04/Program.cs
+++ C43-G05-OOP04/Program.cs
@@ -2,47 +2,85 @@
 
 namespace C43_G05_OOP04
 {
-    class Rectangle
+    //class Rectangle
+    //{
+    //    public int Width { get; set; }
+    //    public int Height { get; set; }
+
+    //    public Rectangle()
+    //    {
+    //        Width = 0;
+    //        Height = 0;
+    //    }
+
+    //    public Rectangle(int width, int height)
+    //    {
+    //        Width = width;
+    //        Height = height;
+    //    }
+
+    //    public Rectangle(int size)
+    //    {
+    //        Width = size;
+    //        Height = size;
+    //    }
+
+    //    public void DisplayDimensions()
+    //    {
+    //        Console.WriteLine($"Width: {Width}, Height: {Height}");
+    //    }
+    //}
+
+
+    class ComplexNumber
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
 
-        public Rectangle()
+        public ComplexNumber(double real, double imaginary)
         {
-            Width = 0;
-            Height = 0;
+            Real = real;
+            Imaginary = imaginary;
         }
 
-        public Rectangle(int width, int height)
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
         {
-            Width = width;
-            Height = height;
+            return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
         }
 
-        public Rectangle(int size)
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
         {
-            Width = size;
-            Height = size;
+            return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
         }
 
-        public void DisplayDimensions()
+        public override string ToString()
         {
-            Console.WriteLine($"Width: {Width}, Height: {Height}");
+            string sign = Imaginary >= 0 ? "+" : "-";
+            return $"{Real} {sign} {Math.Abs(Imaginary)}i";
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Rectangle rect1 = new Rectangle();
-            rect1.DisplayDimensions(); 
 
-            Rectangle rect2 = new Rectangle(10, 20);
-            rect2.DisplayDimensions(); 
+            //    Rectangle rect1 = new Rectangle();
+            //    rect1.DisplayDimensions(); 
 
-            Rectangle rect3 = new Rectangle(15);
-            rect3.DisplayDimensions(); 
+            //    Rectangle rect2 = new Rectangle(10, 20);
+            //    rect2.DisplayDimensions(); 
+
+            //    Rectangle rect3 = new Rectangle(15);
+            //    rect3.DisplayDimensions(); 
+
+            ComplexNumber c1 = new ComplexNumber(3, 4);
+            ComplexNumber c2 = new ComplexNumber(1, 2);
+
+            ComplexNumber sum = c1 + c2;
+            Console.WriteLine($"Sum: {sum}"); 
+
+            ComplexNumber difference = c1 - c2;
+            Console.WriteLine($"Difference: {difference}");
         }
     }
 }
