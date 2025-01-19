8b13f1092cc7dfdb4d8de7d1502df6dc729c3b3b
sayed123 <sayedmohameddev@gmai.com>
2025-01-19 20:07:49 +0200

    Q-01

--- C43-G05-OOP04/Program.cs
+++ C43-G05-OOP04/Program.cs
@@ -2,11 +2,32 @@
 
 namespace C43_G05_OOP04
 {
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Sum of two integers: " + calculator.Add(10, 20)); 
+            Console.WriteLine("Sum of three integers: " + calculator.Add(10, 20, 30)); 
+            Console.WriteLine("Sum of two doubles: " + calculator.Add(5.5, 4.5)); 
         }
     }
 }
